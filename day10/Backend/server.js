216bb9795e5c569b43d4b50cee1cf89b42e97aa5
supriya759694 <supriyabhowmick321@gmail.com>
2026-03-05 13:32:15 +0530

    update api url

--- day10/Backend/server.js
+++ day10/Backend/server.js
@@ -1,12 +1,11 @@
-//server start karna 
-//server database se connect karna
-const dotenv= require('dotenv').config();
-const app = require('./src/app');
-const connectToDB = require('./src/config/database');
+const dotenv = require("dotenv").config();
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
 connectToDB();
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
