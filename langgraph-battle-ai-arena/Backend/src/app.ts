0daaac7acf4cf3e6a1c7ca0f3ac392e8e0fe999f
supriya759694 <supriyabhowmick321@gmail.com>
2026-06-27 11:03:46 +0530

    chore: update dependencies and improve error handling in app.ts
    - Updated @langchain/cohere to version 1.0.5
    - Added @langchain/core and updated @langchain/google-genai to version 2.1.31
    - Updated @langchain/langgraph to version 1.3.2 and @langchain/mistralai to version 1.1.0
    - Updated dotenv to version 17.4.2 and zod to version 4.4.3
    - Added nodemon as a dev dependency
    - Improved error handling in the /invoke endpoint to validate input and catch execution errors

--- langgraph-battle-ai-arena/Backend/src/app.ts
+++ langgraph-battle-ai-arena/Backend/src/app.ts
@@ -3,7 +3,8 @@ import runGraph from "./ai/graph.ai.js"
 import cors from 'cors';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
 app.use(cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
@@ -19,17 +20,32 @@ app.get('/', async (req, res) => {
 })
 
 app.post("/invoke", async (req, res) => {
+try{
+    const { input } = req.body;
+    if(!input || typeof input !== 'string') {
+        return res.status(400).json({
+            message: "Invalid input",
+            success: false
+        });
+    }
 
-    const { input } = req.body
-    const result = await runGraph(input)
+    const result = await runGraph(input);
+    
 
     res.status(200).json({
         message: "Graph executed successfully",
         success: true,
         result
-    })
+    });
 
-})
+} catch (error : any) {
+    res.status(500).json({
+        message: "Error executing graph",
+        success: false,
+        error: error.message
+    });
+}
+});
 
 
 
