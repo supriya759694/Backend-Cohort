72d48349aa5dfe5032f404b870e322d6b28605f1
supriya759694 <supriyabhowmick321@gmail.com>
2026-06-30 20:12:11 +0530

    feat: update CORS configuration to allow multiple origins

--- langgraph-battle-ai-arena/Backend/src/app.ts
+++ langgraph-battle-ai-arena/Backend/src/app.ts
@@ -6,7 +6,10 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+  "http://localhost:5173",
+  "https://backend-cohort-nine.vercel.app"
+],
     methods: ["GET", "POST"],
     credentials: true,
 }))
